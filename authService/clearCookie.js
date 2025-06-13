06c0df5236f0c743335e749c625abf398ed89fc5
Satish Singh <i.sksingh113@gmail.com>
2025-06-14 00:25:11 +0530

    cookie

--- authService/clearCookie.js
+++ authService/clearCookie.js
@@ -1,11 +1,33 @@
+
+//for production
 const { serialize } = require("cookie");
+
 const clearTokenCookie = (res) => {
-  res.setHeader("Set-Cookie", serialize("token", "", {
-    httpOnly: true,
-    secure: false,       
-    sameSite: "lax",      
-    path: "/",            
-    expires: new Date(0), 
-  }));
+  res.setHeader(
+    "Set-Cookie",
+    serialize("token", "", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+      expires: new Date(0),
+    })
+  );
 };
+
 module.exports = clearTokenCookie;
+
+
+
+//for local 
+// const { serialize } = require("cookie");
+// const clearTokenCookie = (res) => {
+//   res.setHeader("Set-Cookie", serialize("token", "", {
+//     httpOnly: true,
+//     secure: false,       
+//     sameSite: "lax",      
+//     path: "/",            
+//     expires: new Date(0), 
+//   }));
+// };
+// module.exports = clearTokenCookie;
--- authService/setTokenCookie.js
+++ authService/setTokenCookie.js
@@ -1,3 +1,5 @@
+
+//for production
 const { serialize } = require("cookie");
 
 const setTokenCookie = (res, token) => {
@@ -5,8 +7,8 @@ const setTokenCookie = (res, token) => {
     "Set-Cookie",
     serialize("token", token, {
       httpOnly: true,
-      secure: false ,
-      sameSite: "lax",
+      secure: true,       
+      sameSite: "none",     
       path: "/",
       maxAge: 60 * 60 * 24, 
     })
@@ -14,3 +16,22 @@ const setTokenCookie = (res, token) => {
 };
 
 module.exports = setTokenCookie;
+
+
+// for local
+//const { serialize } = require("cookie");
+
+// const setTokenCookie = (res, token) => {
+//   res.setHeader(
+//     "Set-Cookie",
+//     serialize("token", token, {
+//       httpOnly: true,
+//       secure: false ,
+//       sameSite: "lax",
+//       path: "/",
+//       maxAge: 60 * 60 * 24, 
+//     })
+//   );
+// };
+
+// module.exports = setTokenCookie;
